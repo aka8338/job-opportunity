2bd5a7a188313bd21cf34a7c68f23840152edb6b
Tekalig <tekahazi06@gmail.com>
2024-10-17 14:21:35 +0300

    implement every function defined by employer.route file

---
+++ server/src/controllers/employer.controller.js
@@ -0,0 +1,177 @@
+const {
+  addCompany,
+  loginCompany,
+  getCompanyProfile,
+  updateCompany,
+  addJob,
+  fatchJobs,
+  updateJob,
+  removeJob,
+  fatchApplicants,
+} = require("../models/employer.model");
+
+const signup = async (req, res) => {
+  try {
+    const { email, password, companyName, companyDescription, contactNumber } =
+      req.body;
+    await addCompany(
+      email,
+      password,
+      companyName,
+      companyDescription,
+      contactNumber
+    )
+      .then((result) => {
+        res.status(201).json({ message: "Company added successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Company not added", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await loginCompany(email, password)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: "Company logged in successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Company not logged in", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const companyProfile = async (req, res) => {
+  try {
+    const { companyId } = req.body;
+    await getCompanyProfile(companyId)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: "Company profile retrieved successfully", result });
+      })
+      .catch((error) => {
+        res
+          .status(400)
+          .json({ message: "Company profile not retrieved", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const editProfile = async (req, res) => {
+  try {
+    const { email, password, companyName } = req.body;
+    await updateCompany(email, password, companyName)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: "Company profile updated successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Company profile not updated", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const postJob = async (req, res) => {
+  try {
+    const { title, description, location, salary } = req.body;
+    await addJob(title, description, location, salary)
+      .then((result) => {
+        res.status(201).json({ message: "Job added successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Job not added", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const getJobs = async (req, res) => {
+  try {
+    const { companyId } = req.body;
+    await fatchJobs(companyId)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: "Jobs retrieved successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Jobs not retrieved", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const editJob = async (req, res) => {
+  try {
+    const { jobId, title, description, location, salary } = req.body;
+    await updateJob(id, title, description, location, salary)
+      .then((result) => {
+        res.status(200).json({ message: "Job updated successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Job not updated", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const deleteJob = async (req, res) => {
+  try {
+    const { jobId } = req.body;
+    await removeJob(jobId)
+      .then((result) => {
+        res.status(200).json({ message: "Job removed successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Job not removed", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const getApplicants = async (req, res) => {
+  try {
+    const { jobId } = req.body;
+    await fatchApplicants(jobId)
+      .then((result) => {
+        res
+          .status(200)
+          .json({ message: "Applicants retrieved successfully", result });
+      })
+      .catch((error) => {
+        res.status(400).json({ message: "Applicants not retrieved", error });
+      });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+  companyProfile,
+  editProfile,
+  postJob,
+  getJobs,
+  editJob,
+  deleteJob,
+  getApplicants,
+};
