52c296adb1483da64037b0acc4964e95d5c26883
Tekalig <tekahazi06@gmail.com>
2024-10-17 14:13:00 +0300

    fixing missing import and naming varibles

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const api = require("./routes/api");
+const dotenv = require("dotenv");
+const apiRouter = require("./routes/api");
 
-const app = express();
 const port = process.env.PORT || 5000;
 
+dotenv.config();
+const app = express();
+
 app.use(express.json());
-app.use("/api", api);
+
+app.use("/api", apiRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
