f62eb0bf5a7d876530497d2c4a3f2b160fe6b3e3
Tekalig <tekahazi06@gmail.com>
2024-10-17 14:11:12 +0300

    creating database connection

---
+++ server/src/config/db.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+// Create a connection to the database
+const connection = mysql.createConnection(dbConfig);
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+});
+
+module.exports = connection;
