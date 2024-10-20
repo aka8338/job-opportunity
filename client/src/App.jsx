4b8baf1fd8bcb4b5bc3ac24eea6e71f12d80f066
Tekalig <tekahazi06@gmail.com>
2024-10-20 21:56:52 +0300

    modify the fly to create a sidebar and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,24 @@
 import { Route, Routes } from "react-router-dom";
-import FloatShape from "./components/FloatShape";
-import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
 import VerifyEmailPage from "./pages/VerifyEmailPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import PostJob from "./components/postJob.jsx";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
-      <FloatShape
-        color="bg-blue-400"
-        size="w-64 h-64"
-        left="10%"
-        top="-5%"
-        delay={0}
-      />
-      <FloatShape
-        color="bg-red-400"
-        size="w-48 h-48"
-        left="70%"
-        top="70%"
-        delay={4}
-      />
-      <FloatShape
-        color="bg-yellow-400"
-        size="w-32 h-32"
-        left="-10%"
-        top="40%"
-        delay={2}
-      />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/verify-email" element={<VerifyEmailPage />} />
-      </Routes>
+    <div className="min-h-screen flex overflow-hidden gap-1">
+      <Sidebar />
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/post-job" element={<PostJob />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/verify-email" element={<VerifyEmailPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
