70f84eb16b9930293082863b717c1e87f3cc467c
Tekalig <tekahazi06@gmail.com>
2024-11-02 00:38:39 +0300

    Refactor database configuration to disable logging

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -11,13 +11,13 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "mysql",
     port: process.env.DB_PORT,
-    logging: (msg) => {
-      if (msg.includes("Executing") || msg.includes("SELECT")) {
-        // Ignore SELECT queries and general SQL executions
-        return;
-      }
-      console.log(msg);
-    },
+    // logging: (msg) => {
+    //   if (msg.includes("Executing") || msg.includes("SELECT")) {
+    //     // Ignore SELECT queries and general SQL executions
+    //     return;
+    //   }
+    //   console.log(msg);
+    // },
   }
 );
 
