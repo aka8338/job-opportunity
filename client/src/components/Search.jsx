918eacc07143c86e662794c6b9ea5e117a98fb12
Tekalig <tekahazi06@gmail.com>
2024-11-02 00:35:47 +0300

    Refactor Search component to improve UI and add post job functionality

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,8 +1,8 @@
-import { Search, CircleX, MapPin, Building } from "lucide-react";
-import AuthStore from "../store/AuthStore";
+import { Search, CircleX, MapPin, Building, Plus } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import AuthStore from "../store/AuthStore";
 function SearchJob() {
-  const { isEmployer } = AuthStore();
   const [searchParams, setSearchParams] = useState({
     jobTitle: "",
     companyName: "",
@@ -10,6 +10,7 @@ function SearchJob() {
     type: "All",
     level: "All",
   });
+  const { isEmployer } = AuthStore();
   const onChange = (e) => {
     const { name, value } = e.target;
     setSearchParams((prevState) => {
@@ -19,10 +20,14 @@ function SearchJob() {
       };
     });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="grid gap-10 rounded-xl bg-gray-400 py-4 px-2">
+    <div className="grid gap-10 rounded-xl bg-gray-500 py-4 text-white px-2">
       <form action="">
-        <div className="flex justify-between items-center gap-3 bg-gray-200 shadow-lg p-5 rounded-lg shadow-gray-600">
+        <div className="flex justify-between items-center gap-3 bg-gray-400 text-black shadow-lg p-5 rounded-lg shadow-gray-600">
           <div className="flex gap-2 items-center">
             <Search className="cursor-pointer text-[25px]" />
             <input
@@ -33,46 +38,42 @@ function SearchJob() {
               className="bg-transparent w-full focus:outline-none"
               placeholder="Search Job title here..."
             />
-            {!isEmployer && (
-              <CircleX className="text-[30px] hover:text-gray-800 cursor-pointer" />
-            )}
+            <CircleX className="text-[30px] hover:text-gray-800 cursor-pointer" />
           </div>
-          {!isEmployer && (
-            <div className="flex gap-2 items-center">
-              <Building className="cursor-pointer text-[25px]" />
-              <input
-                type="text"
-                name="companyName"
-                value={searchParams.companyName}
-                onChange={onChange}
-                className="bg-transparent w-full focus:outline-none"
-                placeholder="Search by Company..."
-              />
-              <CircleX className="text-[30px] hover:text-gray-800 cursor-pointer" />
-            </div>
-          )}
-          {!isEmployer && (
-            <div className="flex gap-2 items-center">
-              <MapPin className="cursor-pointer text-[25px]" />
-              <input
-                type="text"
-                name="location"
-                value={searchParams.location}
-                onChange={onChange}
-                className="bg-transparent w-full focus:outline-none"
-                placeholder="Search by Location..."
-              />
-              <CircleX className="text-[30px] hover:text-gray-800 cursor-pointer" />
-            </div>
-          )}
-
-          <button className="px-10 rounded-xl bg-amber-600 h-full p-2 text-white">
+          <div className="flex gap-2 items-center">
+            <Building className="cursor-pointer text-[25px]" />
+            <input
+              type="text"
+              name="companyName"
+              value={searchParams.companyName}
+              onChange={onChange}
+              className="bg-transparent w-full focus:outline-none"
+              placeholder="Search by Company..."
+            />
+            <CircleX className="text-[30px] hover:text-gray-800 cursor-pointer" />
+          </div>
+          <div className="flex gap-2 items-center">
+            <MapPin className="cursor-pointer text-[25px]" />
+            <input
+              type="text"
+              name="location"
+              value={searchParams.location}
+              onChange={onChange}
+              className="bg-transparent w-full focus:outline-none"
+              placeholder="Search by Location..."
+            />
+            <CircleX className="text-[30px] hover:text-gray-800 cursor-pointer" />
+          </div>
+          <button
+            className="px-10 rounded-xl bg-amber-600 h-full p-2 text-white"
+            onClick={handleSubmit}
+          >
             Search
           </button>
         </div>
       </form>
 
-      <div className="flex gap-10 justify-center items-center">
+      <div className="flex gap-10 justify-center items-center text-black">
         <div className="flex gap-2 items-center">
           <label className="text-semibold" htmlFor="type">
             Type:
@@ -109,6 +110,16 @@ function SearchJob() {
           </select>
         </div>
       </div>
+      {isEmployer && (
+        <Link className="flex justify-center mt-5" to={"/postJob"}>
+          <span className="text-md font-bold p-2 underline">
+            Post a New Job
+          </span>
+          <button className="p-2 mx-4 rounded-xl bg-green-600 text-black hover:bg-green-300">
+            <Plus />
+          </button>
+        </Link>
+      )}
     </div>
   );
 }
