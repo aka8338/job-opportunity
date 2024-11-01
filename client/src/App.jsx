78bfd6ef631735c694163323228305f90af91985
Tekalig <tekahazi06@gmail.com>
2024-11-02 00:32:19 +0300

    Refactor component imports and add dark mode selector

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,56 @@
 import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
 
-import NavBar from './components/NavBar.jsx';
-
-import JobDetails from "./components/jobdetails.jsx";
+import ApplicantStatus from "./components/applicantStatus.jsx";
 import PostJob from "./components/postJob.jsx";
 import AboutUs from "./pages/About.jsx";
 import ContactUs from "./pages/ContactUs.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
-import EmployerDashboard from "./pages/EmployerHome.jsx";
 import EmployerSignup from "./pages/EmployerSignup.jsx";
-import ExpertHomePage from "./pages/ExpertHome.jsx";
+import HomePage from "./pages/HomePage.jsx";
 import { default as ExpertSignUp } from "./pages/ExpertSignup.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import ProfilePage from "./pages/Profile.jsx";
 import VerifyEmailPage from "./pages/VerifyEmailPage.jsx";
 import AuthStore from "./store/AuthStore";
+import ApplyJob from "./components/applyJob.jsx";
 
 function App() {
   const { isAuthenticated } = AuthStore();
 
-
   return (
-    <div className="min-h-screen flex overflow-hidden gap-1">
-      <div className="flex-1">
-      <NavBar />
-        <Routes>
+    <div className="dark">
+      <div className="bg-white text-black dark:bg-gray-800 dark:text-white min-h-screen flex flex-col overflow-hidden space-y-16">
+        <NavBar />
+        <div className="flex-1">
+          <Routes>
+            <Route
+              path="/"
+              element={isAuthenticated ? <HomePage /> : <Home />}
+            />
+            {/* {Job routes } */}
+            <Route path="/jobs" element={<Dashboard />} />
+            <Route path="/postJob" element={<PostJob />} />
+            <Route path="/applyJob/:jobId" element={<ApplyJob />} />
+            <Route
+              path="/applicantStatus/:jobId"
+              element={<ApplicantStatus />}
+            />
+            {/* {Auth routes} */}
+            <Route path="/expert/signup" element={<ExpertSignUp />} />
+            <Route path="/employer/signup" element={<EmployerSignup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path={`/verify`} element={<VerifyEmailPage />} />
 
-         
-          <Route
-            path="/"
-            element={isAuthenticated ? <Dashboard /> : <Home />} />
-
-=======
-          <Route path="/jobs" element={<Dashboard />} />
-
-          <Route path="/post-job" element={<PostJob />} />
-          <Route path="/jobDetails" element={<JobDetails />} />
-
-          <Route path="/expert/signup" element={<ExpertSignUp />} />
-          <Route path="/employer/signup" element={<EmployerSignup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path={`/verify`} element={<VerifyEmailPage />} />
-          <Route
-            path="/expertHome"
-            element={isAuthenticated ? <ExpertHomePage /> : <Home />}
-          />
-          <Route path="/profile" element={<ProfilePage />} />
-
-          <Route path="/employerHome" element={<EmployerDashboard />} />
-
-          <Route
-            path="/employerHome"
-            element={isAuthenticated ? <EmployerDashboard /> : <Home />}
-          />
-
-        </Routes>
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
