ec1fcd1700fade7cfc84409892c8c8cfbcd13dfe
Tekalig <tekahazi06@gmail.com>
2024-11-02 00:39:58 +0300

    Add logout route to API

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -7,5 +7,8 @@ const apiRouter = express.Router();
 
 apiRouter.use("/expert", verifyToken, expertRouter);
 apiRouter.use("/employer", verifyToken, employerRouter);
+apiRouter.get("/logout", (req, res) => {
+  res.clearCookie("token").send("Logged out successfully");
+});
 
 module.exports = apiRouter;
