5f841faaad58be8a55afef514c9cc3e4350cdbe2
Tekalig <tekahazi06@gmail.com>
2024-11-02 00:35:30 +0300

    Refactor PostJob component to use controlled inputs and add edit functionality

--- client/src/components/postJob.jsx
+++ client/src/components/postJob.jsx
@@ -1,45 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AuthStore from "../store/AuthStore";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const PostJob = () => {
-  const [jobTitle, setJobTitle] = useState("");
-  const [jobDescription, setJobDescription] = useState("");
-  const [requiredSkills, setRequiredSkills] = useState("");
-  const [jobLocation, setJobLocation] = useState("");
-  const [salary, setSalary] = useState("");
-  const [jobType, setJobType] = useState("");
-  const [jobLevel, setJobLevel] = useState("");
+  const [jobData, setPostData] = useState({
+    jobTitle: "",
+    jobDescription: "",
+    jobSkills: "",
+    jobLocation: "",
+    jobSalary: "",
+    jobType: "",
+    jobLevel: "",
+    jobExperience: "",
+  });
 
-  const { postJob } = AuthStore();
+  const { postJob, jobs, editJob } = AuthStore();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { jobId } = location.state || {};
+
+  useEffect(() => {
+    if (jobId) {
+      const job = jobs.filter((job) => job.jobId === jobId)[0];
+      setPostData(job);
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPostData({
+      ...jobData,
+      [name]: value,
+    });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const jobData = {
-      jobTitle,
-      jobDescription,
-      requiredSkills,
-      jobLocation,
-      salary,
-      jobType,
-      jobLevel,
-    };
-    await postJob(jobData);
-    console.log("Job Posted:", jobData);
-    // Add your form submission logic here
+    if (!jobId) {
+      await postJob(jobData);
+    } else {
+      await editJob(jobData, jobId);
+    }
+    navigate("/jobs");
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-lg mx-auto p-6 bg-amber-300 shadow-md rounded-md"
+      className="max-w-lg mx-auto p-6 bg-slate-300 text-black shadow-md rounded-md"
     >
       <div className="mb-4">
         <label className="block text-gray-700 font-bold mb-2">Job Title:</label>
         <input
           type="text"
-          value={jobTitle}
-          onChange={(e) => setJobTitle(e.target.value)}
+          name="jobTitle"
+          value={jobData.jobTitle}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
         />
       </div>
       <div className="mb-4">
@@ -47,9 +65,11 @@ const PostJob = () => {
           Job Description:
         </label>
         <textarea
-          value={jobDescription}
-          onChange={(e) => setJobDescription(e.target.value)}
+          name="jobDescription"
+          value={jobData.jobDescription}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
         />
       </div>
       <div className="mb-4">
@@ -58,9 +78,11 @@ const PostJob = () => {
         </label>
         <input
           type="text"
-          value={requiredSkills}
-          onChange={(e) => setRequiredSkills(e.target.value)}
+          name="jobSkills"
+          value={jobData.jobSkills}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
         />
       </div>
       <div className="mb-4">
@@ -69,44 +91,74 @@ const PostJob = () => {
         </label>
         <input
           type="text"
-          value={jobLocation}
-          onChange={(e) => setJobLocation(e.target.value)}
+          name="jobLocation"
+          value={jobData.jobLocation}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
         />
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 font-bold mb-2">Salary:</label>
         <input
           type="text"
-          value={salary}
-          onChange={(e) => setSalary(e.target.value)}
+          name="jobSalary"
+          value={jobData.jobSalary}
+          onChange={handleChange}
           className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2">
+          Required Experience:
+        </label>
+        <input
+          type="text"
+          name="jobExperience"
+          value={jobData.jobExperience}
+          onChange={handleChange}
+          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
         />
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 font-bold mb-2">Job Type:</label>
-        <input
-          type="text"
-          value={jobType}
-          onChange={(e) => setJobType(e.target.value)}
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+        <select
+          name="jobType"
+          value={jobData.jobType}
+          onChange={handleChange}
+          className="w-full px-3 py-2 text-black border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        >
+          <option value="">Select Job Type</option>
+          <option value="fulltime">Full-time</option>
+          <option value="parttime">Part-time</option>
+          <option value="contract">Contract</option>
+          <option value="internship">Internship</option>
+        </select>
       </div>
 
       <div className="mb-4">
         <label className="block text-gray-700 font-bold mb-2">Job Level:</label>
-        <input
-          type="text"
-          value={jobLevel}
-          onChange={(e) => setJobLevel(e.target.value)}
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+        <select
+          name="jobLevel"
+          value={jobData.jobLevel}
+          onChange={handleChange}
+          className="w-full px-3 py-2 border text-black rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        >
+          <option value="">Select Job Level</option>
+          <option value="entry">Entry</option>
+          <option value="intermediate">Mid</option>
+          <option value="senior">Senior</option>
+        </select>
       </div>
       <button
         type="submit"
         className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
-        Post Job
+        {jobId ? "Save" : "Post"}
       </button>
     </form>
   );
